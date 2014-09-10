365b1ba500378270afcb9ddee1ddbf1b1aee53b3
paulrajah <pr257@njit.edu>
2014-09-10 10:58:31 -0400

    added some other files

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYP html>
+<head>
+
+    <title>Paul Rajah - IS218</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+
+    <section>
+
